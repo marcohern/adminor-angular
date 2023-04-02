83089eca72d4848a172cbee9a34a26588a2fec1d
Marco Hernandez <marcohern@gmail.com>
2023-04-02 02:25:27 -0500

    set environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,4 +9,9 @@ export const environment = {
     appId: '************************************',
     measurementId: '************************************',
   },
+  prefix: 'http://server.com',
+  local:  {
+    clientId: '98d52d01-6448-4513-a73b-4d0703985065',
+    clientSecret: 'NrEtGC9ee0asucErb593QnCXMTQpTvWpNP3x6FPO'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,6 +13,11 @@ export const environment = {
     appId: '************************************',
     measurementId: '************************************',
   },
+  prefix: 'http://localhost:8000',
+  local:  {
+    clientId: '98d52d01-6448-4513-a73b-4d0703985065',
+    clientSecret: 'NrEtGC9ee0asucErb593QnCXMTQpTvWpNP3x6FPO'
+  }
 };
 
 /*
