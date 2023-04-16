563525126d5730f0bbc15500438e56d1a81c58a4
Marco Hernandez <marcohern@gmail.com>
2023-04-16 11:57:32 -0500

    added icon support for counter2

--- src/app/marcohern/shared/counters/counter2/counter2.component.ts
+++ src/app/marcohern/shared/counters/counter2/counter2.component.ts
@@ -8,6 +8,7 @@ import { Component, Input } from '@angular/core';
 export class Counter2Component {
   @Input() public bg:string = 'bg-primary shadow-primary';
   @Input() public status:string = 'bg-success-transparent tx-success';
+  @Input() public icon:string = 'bx-user';
   @Input() public title:string = '*Title*';
   @Input() public amount:number = 0;
   @Input() public date:Date = new Date('2023-04-16T11:30');
